c37dc95714db2d0291b1af8a4e37073eb7a7ca3f
Dao Tran <daotran@ankvn.com>
2025-12-10 17:12:03 +0700

    .

--- lib/screens/my_vip_list/widgets/my_vip_list_item.dart
+++ lib/screens/my_vip_list/widgets/my_vip_list_item.dart
@@ -40,205 +40,142 @@ class MyVipListItem extends StatelessWidget {
             borderRadius: BorderRadius.circular(10.0.sp),
           ),
           padding: EdgeInsets.fromLTRB(10.0.sp, 20.0.sp, 10.0.sp, 10.0.sp),
-          child: Stack(children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  "assets/images/logo_background.png",
-                  fit: BoxFit.fitHeight,
-                  height: 84.0.sp,
-                ),
-              ],
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "${vipMemberItem!.shopName}",
-                  style: appTheme.textTheme.headlineLarge!.copyWith(
-                    color: appTheme.colorScheme.onPrimary,
-                    fontWeight: FontWeight.bold,
+          child: Stack(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/images/logo_background.png",
+                    fit: BoxFit.fitHeight,
+                    height: 84.0.sp,
                   ),
-                ),
-                SizedBox(height: 45.0.sp),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Icon(
-                      Icons.location_on_outlined,
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "${vipMemberItem!.shopName}",
+                    style: appTheme.textTheme.headlineLarge!.copyWith(
                       color: appTheme.colorScheme.onPrimary,
-                      size: 18.0.sp,
+                      fontWeight: FontWeight.bold,
                     ),
-                    SizedBox(width: 5.0.sp),
-                    Expanded(
-                      child: Text(
-                        "${vipMemberItem!.shopAddress}",
-                        style: appTheme.textTheme.headlineSmall!
-                            .copyWith(color: appTheme.colorScheme.onPrimary),
+                  ),
+                  SizedBox(height: 45.0.sp),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.location_on_outlined,
+                        color: appTheme.colorScheme.onPrimary,
+                        size: 18.0.sp,
                       ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 5.0.sp),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.date_range,
-                      color: appTheme.colorScheme.onPrimary,
-                      size: 18.0.sp,
-                    ),
-                    SizedBox(width: 5.0.sp),
-                    vipMemberItem!.typeLimit == VipMemberType.UNLIMIT
-                        ? Obx(() => Text(
-                            _autoRenew.value
-                                ? "${"next_renewal_date".tr}: ${vipMemberItem!.toDate!.toStringFormatSimpleDateUTC()}"
-                                : "${"expiration_date".tr}: ${vipMemberItem!.toDate!.toStringFormatSimpleDateUTC()}",
-                            style: appTheme.textTheme.headlineSmall!.copyWith(
-                                color: appTheme.colorScheme.onPrimary)))
-                        : Text(
+                      SizedBox(width: 5.0.sp),
+                      Expanded(
+                        child: Text(
+                          "${vipMemberItem!.shopAddress}",
+                          style: appTheme.textTheme.headlineSmall!.copyWith(
+                            color: appTheme.colorScheme.onPrimary,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 5.0.sp),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.date_range,
+                        color: appTheme.colorScheme.onPrimary,
+                        size: 18.0.sp,
+                      ),
+                      SizedBox(width: 5.0.sp),
+                      vipMemberItem!.typeLimit == VipMemberType.UNLIMIT
+                          ? Obx(
+                            () => Text(
+                              _autoRenew.value
+                                  ? "${"next_renewal_date".tr}: ${vipMemberItem!.toDate!.toStringFormatSimpleDateUTC()}"
+                                  : "${"expiration_date".tr}: ${vipMemberItem!.toDate!.toStringFormatSimpleDateUTC()}",
+                              style: appTheme.textTheme.headlineSmall!.copyWith(
+                                color: appTheme.colorScheme.onPrimary,
+                              ),
+                            ),
+                          )
+                          : Text(
                             "${"from_date".tr.replaceFirst('...', "${vipMemberItem!.getLimitMemberFromDate()!.toStringFormatSimpleDateUTC()}")}" +
                                 " -- " +
                                 "${"to_date".tr.replaceFirst('...', "${vipMemberItem!.getLimitMemberToDate()!.toStringFormatSimpleDateUTC()}")}",
                             style: appTheme.textTheme.headlineSmall!.copyWith(
-                                color: appTheme.colorScheme.onPrimary)),
-                  ],
-                ),
-                SizedBox(height: 5.0.sp),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.play_arrow_outlined,
-                      color: appTheme.colorScheme.onPrimary,
-                      size: 18.0.sp,
-                    ),
-                    SizedBox(width: 5.0.sp),
-                    Text(
-                      (vipMemberItem!.typeLimit == VipMemberType.UNLIMIT
-                          ? "${"unlimited_turns".tr}"
-                          : ("${"available_turns".tr}: " +
-                              "${vipMemberItem?.remainPlay} / ${vipMemberItem?.sumBuyPlay}")),
-                      style: appTheme.textTheme.headlineSmall!
-                          .copyWith(color: appTheme.colorScheme.onPrimary),
-                    ),
-                  ],
-                ),
-                vipMemberItem!.typeLimit == VipMemberType.UNLIMIT
-                    ? Obx(
-                        () => Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            if (_autoRenew.value)
-                              PressableText(
-                                "cancel_auto_renew".tr,
-                                backgroundColor: appTheme.colorScheme.error
-                                    .withOpacity(.8),
-                                borderSide: BorderSide.none,
-                                borderRadius:
-                                    BorderRadius.circular(15.0.sp),
-                                padding: EdgeInsets.symmetric(
-                                  vertical: 5.0.sp,
-                                  horizontal: 10.0.sp,
-                                ),
-                                style:
-                                    appTheme.textTheme.titleSmall!.copyWith(
-                                  color: appTheme.colorScheme.onError,
-                                  fontSize: 9.0.sp,
-                                ),
-                                onPress: () {
-                                  SupportUtils.showDecisionDialog(
-                                      "${"are_your_sure_cancel_auto_renew".tr} ",
-                                      lstOptions: [
-                                        DecisionOption(
-                                          'yes'.tr,
-                                          onDecisionPressed: () {
-                                            _autoRenew.value =
-                                                !_autoRenew.value;
-                                            autoRenewChanged
-                                                ?.call(_autoRenew.value);
-                                          },
-                                        ),
-                                        DecisionOption('no'.tr,
-                                            isImportant: true,
-                                            type: DecisionOptionType.DENIED,
-                                            onDecisionPressed: null),
-                                      ]);
+                              color: appTheme.colorScheme.onPrimary,
+                            ),
+                          ),
+                    ],
+                  ),
+                  SizedBox(height: 5.0.sp),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.play_arrow_outlined,
+                        color: appTheme.colorScheme.onPrimary,
+                        size: 18.0.sp,
+                      ),
+                      SizedBox(width: 5.0.sp),
+                      Text(
+                        (vipMemberItem!.typeLimit == VipMemberType.UNLIMIT
+                            ? "${"unlimited_turns".tr}"
+                            : ("${"available_turns".tr}: " +
+                                "${vipMemberItem?.remainPlay} / ${vipMemberItem?.sumBuyPlay}")),
+                        style: appTheme.textTheme.headlineSmall!.copyWith(
+                          color: appTheme.colorScheme.onPrimary,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      PressableText(
+                        "cancel".tr,
+                        backgroundColor: appTheme.colorScheme.error.withOpacity(
+                          .85,
+                        ),
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(15.0.sp),
+                        padding: EdgeInsets.symmetric(
+                          vertical: 5.0.sp,
+                          horizontal: 10.0.sp,
+                        ),
+                        style: appTheme.textTheme.titleSmall!.copyWith(
+                          color: appTheme.colorScheme.onError,
+                          fontSize: 9.0.sp,
+                        ),
+                        onPress: () {
+                          SupportUtils.showDecisionDialog(
+                            "${"membership_cancel_notice".tr}",
+                            lstOptions: [
+                              DecisionOption(
+                                'withdraw_membership'.tr,
+                                onDecisionPressed: () {
+                                  cancelPressed?.call();
                                 },
                               ),
-                            if (_autoRenew.value) SizedBox(width: 8.0.sp),
-                            PressableText(
-                              "cancel".tr,
-                              backgroundColor:
-                                  appTheme.colorScheme.error.withOpacity(.85),
-                              borderSide: BorderSide.none,
-                              borderRadius: BorderRadius.circular(15.0.sp),
-                              padding: EdgeInsets.symmetric(
-                                vertical: 5.0.sp,
-                                horizontal: 10.0.sp,
+                              DecisionOption(
+                                'cancel'.tr,
+                                isImportant: true,
+                                type: DecisionOptionType.DENIED,
+                                onDecisionPressed: null,
                               ),
-                              style: appTheme.textTheme.titleSmall!.copyWith(
-                                color: appTheme.colorScheme.onError,
-                                fontSize: 9.0.sp,
-                              ),
-                              onPress: () {
-                                SupportUtils.showDecisionDialog(
-                                    "${"membership_cancel_notice".tr}",
-                                    lstOptions: [
-                                      DecisionOption(
-                                        'withdraw_membership'.tr,
-                                        onDecisionPressed: () {
-                                          cancelPressed?.call();
-                                        },
-                                      ),
-                                      DecisionOption('cancel'.tr,
-                                          isImportant: true,
-                                          type: DecisionOptionType.DENIED,
-                                          onDecisionPressed: null),
-                                    ]);
-                              },
-                            ),
-                          ],
-                        ),
-                      )
-                    : Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          PressableText(
-                            "cancel".tr,
-                            backgroundColor:
-                                appTheme.colorScheme.error.withOpacity(.85),
-                            borderSide: BorderSide.none,
-                            borderRadius: BorderRadius.circular(15.0.sp),
-                            padding: EdgeInsets.symmetric(
-                              vertical: 5.0.sp,
-                              horizontal: 10.0.sp,
-                            ),
-                            style: appTheme.textTheme.titleSmall!.copyWith(
-                              color: appTheme.colorScheme.onError,
-                              fontSize: 9.0.sp,
-                            ),
-                            onPress: () {
-                              SupportUtils.showDecisionDialog(
-                                  "${"membership_cancel_notice".tr}",
-                                  lstOptions: [
-                                    DecisionOption(
-                                      'withdraw_membership'.tr,
-                                      onDecisionPressed: () {
-                                        cancelPressed?.call();
-                                      },
-                                    ),
-                                    DecisionOption('cancel'.tr,
-                                        isImportant: true,
-                                        type: DecisionOptionType.DENIED,
-                                        onDecisionPressed: null),
-                                  ]);
-                            },
-                          ),
-                        ],
+                            ],
+                          );
+                        },
                       ),
-              ],
-            ),
-          ]),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
         onPress: () => itemPressed?.call(),
       ),
