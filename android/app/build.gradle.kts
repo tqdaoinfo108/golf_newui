3839642bb4fd6a5fbad3e7e88629feddf06bb517
Dao Tran <qdao.cntt@gmail.com>
2025-12-15 16:58:00 +0700

    .
    .

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
 android {
     namespace = "jp.gpro.golf24"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "29.0.14033849"
+    ndkVersion = "29.0.14206865"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
